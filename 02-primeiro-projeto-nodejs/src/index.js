727c0f8db3002027aa2405aa5bc142333e2cf1ba
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-04 14:45:31 -0300

    Cadastro de conta

--- 02-primeiro-projeto-nodejs/src/index.js
+++ 02-primeiro-projeto-nodejs/src/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const port = 3333;
 const app = express();
+
+const customers = [];
+
+app.use(express.json());
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send()
+});
 
 app.listen(port, () => {
   console.log(`Active app at: http://localhost:${port}`)
