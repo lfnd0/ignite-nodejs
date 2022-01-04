fcde48429dc74bd40e22068c2b546b52082dd8b2
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-04 17:17:59 -0300

    Criando saque na conta

--- 02-primeiro-projeto-nodejs/README.md
+++ 02-primeiro-projeto-nodejs/README.md
@@ -18,7 +18,7 @@
 - [x] Deve ser possível criar uma conta.
 - [x] Deve ser possível buscar o extrato bancário do cliente.
 - [x] Deve ser possível realizar um depósito.
-- [ ] Deve ser possível realizar um saque.
+- [x] Deve ser possível realizar um saque.
 - [ ] Deve ser possível buscar o extrato bancário do cliente por data.
 - [ ] Deve ser possível atualizar dados da conta do cliente.
 - [ ] Deve ser possível obter dados da conta do cliente.
@@ -31,6 +31,6 @@
 - [x] Não dever ser possível cadastrar uma conta com CPF já existente.
 - [x] Não dever ser possível buscar extrato em uma conta não existente.
 - [x] Não dever ser possível fazer depósito em uma conta não existente.
-- [ ] Não dever ser possível fazer saque em uma conta não existente.
-- [ ] Não dever ser possível fazer saque quando o saldo for insuficiente.
+- [x] Não dever ser possível fazer saque em uma conta não existente.
+- [x] Não dever ser possível fazer saque quando o saldo for insuficiente.
 - [ ] Não dever ser possível excluir uma conta não existente.
--- 02-primeiro-projeto-nodejs/src/index.js
+++ 02-primeiro-projeto-nodejs/src/index.js
@@ -12,12 +12,24 @@ function verifyIfExistsAccountCPF(request, response, next) {
   const customer = customers.find(customer => customer.cpf === cpf);
 
   if (!customer) {
-    return response.status(400).json({ 'error': 'Customer not found!' })
+    return response.status(400).json({ error: 'Customer not found!' })
   }
 
   request.customer = customer;
 
   return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
 }
 
 app.use(express.json());
@@ -59,6 +71,27 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     amount,
     created_at: new Date(),
     type: 'credit'
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { amount } = request.body;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insuficient funds!' })
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
   };
 
   customer.statement.push(statementOperation);
