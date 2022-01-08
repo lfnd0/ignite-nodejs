92d4be6c6fabb387fb786127f730b419003baa17
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-08 01:10:21 -0300

    Criando categoria

---
+++ 03-iniciando-api/src/routes/categories.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const categoriesRouter = Router();
+const categories = [];
+
+categoriesRouter.post("/categories", (request, response) => {
+  const { name, description } = request.body;
+
+  categories.push({
+    name,
+    description,
+  });
+
+  return response.status(201).send();
+});
+
+export { categoriesRouter };
--- 03-iniciando-api/src/server.ts
+++ 03-iniciando-api/src/server.ts
@@ -1,19 +1,12 @@
 import express from "express";
 
+import { categoriesRouter } from "./routes/categories.routes";
+
 const port = 3333;
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world!" });
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use(categoriesRouter);
 
 app.listen(port, () => {
   console.log(`Active app at: http://localhost:${port}`);
