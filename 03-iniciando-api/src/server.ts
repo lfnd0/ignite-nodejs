7dd4cc9ce4c6ac5a737716b7073cbba8de0fcd75
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-08 00:45:48 -0300

    Debugando a aplicação

--- 03-iniciando-api/src/server.ts
+++ 03-iniciando-api/src/server.ts
@@ -3,8 +3,16 @@ import express from "express";
 const port = 3333;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello world!" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(port, () => {
