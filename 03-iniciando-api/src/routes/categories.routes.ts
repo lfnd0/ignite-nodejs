9132eb1b7115dc351478da25ffdeb298c4101551
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-08 21:23:37 -0300

    Criando repositório de categoria

--- 03-iniciando-api/src/routes/categories.routes.ts
+++ 03-iniciando-api/src/routes/categories.routes.ts
@@ -1,23 +1,16 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRouter = Router();
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  categoriesRepository.create({ name, description });
 
-  categories.push(category);
-
-  return response.status(201).json({ category });
+  return response.status(201).send();
 });
 
 export { categoriesRouter };
