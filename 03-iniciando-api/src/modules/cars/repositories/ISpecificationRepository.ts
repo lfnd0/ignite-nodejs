4d524c577b81885f98cdf32e554f36a122bae949
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-11 11:01:15 -0300

    Criando service de especificação e separando em módulos

---
+++ 03-iniciando-api/src/modules/cars/repositories/ISpecificationRepository.ts
@@ -0,0 +1,3 @@
+interface ISpecificationRepository {}
+
+export { ISpecificationRepository };
--- 03-iniciando-api/src/routes/categories.routes.ts
+++ 03-iniciando-api/src/routes/categories.routes.ts
@@ -1,24 +1,22 @@
 import { Router } from "express";
 
-import { PostgreSQLCategoriesRepository } from "../repositories/PostgreSQLCategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRouter = Router();
-const postgreSQLCategoriesRepository = new PostgreSQLCategoriesRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const createCategoryService = new CreateCategoryService(
-    postgreSQLCategoriesRepository
-  );
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
   createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
 
 categoriesRouter.get("/", (request, response) => {
-  const listCategories = postgreSQLCategoriesRepository.list();
+  const listCategories = categoriesRepository.list();
 
   return response.json(listCategories);
 });
