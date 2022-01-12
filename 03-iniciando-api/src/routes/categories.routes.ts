36b0bf7c0fa9074b891facfef9a8dabe73ce485f
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-12 17:10:59 -0300

    Refatorando a listagem de categoria

--- 03-iniciando-api/src/routes/categories.routes.ts
+++ 03-iniciando-api/src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRouter = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRouter.get("/", (request, response) => {
-  const listCategories = categoriesRepository.list();
-
-  return response.json(listCategories);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRouter };
