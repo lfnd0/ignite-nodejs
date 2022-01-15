01d272cef1271dc4811b3bc24ed0e38b74a2045b
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-14 21:37:21 -0300

    Configurando o Swagger

--- 03-iniciando-api/src/server.ts
+++ 03-iniciando-api/src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const port = 3333;
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(port, () => {
   console.log(`Active app at: http://localhost:${port}`);
