4dda36062d5755b47708d03baf3534bfa51f2312
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-26 00:19:46 -0300

    Criando migration de usuário (update)

--- 04-continuando-aplicacao/src/modules/accounts/entities/User.ts
+++ 04-continuando-aplicacao/src/modules/accounts/entities/User.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("users")
 class User {
   @PrimaryColumn()
   id: string;
