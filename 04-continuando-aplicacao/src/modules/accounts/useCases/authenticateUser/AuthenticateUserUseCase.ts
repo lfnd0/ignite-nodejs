3aa736cf298f85ec823a5a572c2415f7e10a337a
luizfernando <luiz-fsilva37@hotmail.com>
2022-01-27 17:57:12 -0300

    Tratamento de exceções

--- 04-continuando-aplicacao/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ 04-continuando-aplicacao/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -29,13 +30,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("E-mail or password incorrect!");
+      throw new AppError("E-mail or password incorrect!");
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error("E-mail or password incorrect!");
+      throw new AppError("E-mail or password incorrect!");
     }
 
     const token = sign({}, "a7e071b3de48cec1dd24de6cbe6c7bf1", {
--- 04-continuando-aplicacao/src/server.ts
+++ 04-continuando-aplicacao/src/server.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -11,7 +13,23 @@ const port = 3333;
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error: ${error.message}`,
+    });
+  }
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
