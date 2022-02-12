4d9f34f5990c2883f00c2f9542825043c1608f9d
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-12 18:41:51 -0300

    Criando middleware de administrador

--- 05-testes-regras-negocio/src/shared/infra/http/routes/cars.routes.ts
+++ 05-testes-regras-negocio/src/shared/infra/http/routes/cars.routes.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const carRouter = Router();
 
 const createCarController = new CreateCarController();
 
-carRouter.post("/", createCarController.handle);
+carRouter.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carRouter };
--- 05-testes-regras-negocio/src/shared/infra/http/routes/categories.routes.ts
+++ 05-testes-regras-negocio/src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,9 @@ import { CreateCategoryController } from "@modules/cars/useCases/createCategory/
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const categoriesRouter = Router();
 
 const upload = multer({
@@ -15,13 +18,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRouter.post("/", createCategoryController.handle);
+categoriesRouter.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 categoriesRouter.get("/", listCategoriesController.handle);
 
 categoriesRouter.post(
   "/import",
   upload.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
