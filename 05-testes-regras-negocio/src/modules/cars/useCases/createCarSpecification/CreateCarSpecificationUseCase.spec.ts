7e679ae404dde3c0a61ca7c719460704a011e95d
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-14 16:57:11 -0300

    Continuação do CreateCarSpecificationUseCase

--- 05-testes-regras-negocio/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ 05-testes-regras-negocio/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,16 +1,22 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { SpecificationsRepositoryInMemory } from "@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 describe("Create car specification", () => {
-  carsRepositoryInMemory = new CarsRepositoryInMemory();
-  createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
-    carsRepositoryInMemory
-  );
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory
+    );
+  });
 
   it("Should not be able to add a new specification to a now-existent car.", async () => {
     expect(async () => {
