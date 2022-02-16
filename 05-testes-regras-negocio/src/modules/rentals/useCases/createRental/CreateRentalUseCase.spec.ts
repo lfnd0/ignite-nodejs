0de9b385c400ba439a8f5c7fbfd9819a932ff3fb
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-16 18:14:56 -0300

    Continuação do cadastro de aluguel

--- 05-testes-regras-negocio/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ 05-testes-regras-negocio/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
@@ -12,10 +13,45 @@ describe("Create rental", () => {
   });
 
   it("Should be able to create a new rental.", async () => {
-    await createRentalUseCase.execute({
+    const rental = await createRentalUseCase.execute({
       user_id: "4123",
       car_id: "9330",
       expected_return_date: new Date(),
     });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("Should not be able to create a new rental if there is another open to the same user.", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "4123",
+        car_id: "9330",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "4123",
+        car_id: "9330",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to create a new rental if there is another open to the same car.", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "6532",
+        car_id: "4120",
+        expected_return_date: new Date(),
+      });
+
+      await createRentalUseCase.execute({
+        user_id: "3221",
+        car_id: "4120",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
