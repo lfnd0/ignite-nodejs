7a943b2013184a66c996d0a6f47881e6151aea6d
luizfernando <luiz-fsilva37@hotmail.com>
2022-02-16 17:45:03 -0300

    Criando os testes do aluguel

---
+++ 05-testes-regras-negocio/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,21 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let createRentalUseCase: CreateRentalUseCase;
+
+describe("Create rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("Should be able to create a new rental.", async () => {
+    await createRentalUseCase.execute({
+      user_id: "4123",
+      car_id: "9330",
+      expected_return_date: new Date(),
+    });
+  });
+});
